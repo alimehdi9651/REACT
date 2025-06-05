613b9b269e53a565acaeca985714ae4efac9f488
alimehdi <alimehdi9651@gmail.com>
2025-06-05 16:06:49 +0530

    ep1 end

--- App.js
+++ App.js
@@ -13,10 +13,14 @@ const root = ReactDOM.createRoot(document.getElementById("root"))
 
 const parent = React.createElement("div",{id: "parent"},
 
-    React.createElement("div",{id: "child"},
+   [ React.createElement("div",{id: "child"},
+    //for multiple children we simply have to put each child into an array
+    [React.createElement("h1",{},"i am h1 tag"),React.createElement("h2",{},"i am h2 tag")]),
+    React.createElement("div",{id: "child2"},
         //for multiple children we simply have to put each child into an array
-        [React.createElement("h1",{},"i am h1 tag"),React.createElement("h2",{},"i am h2 tag")]))
+        [React.createElement("h1",{},"i am h1 tag2"),React.createElement("h2",{},"i am h2 tag2")])]
+)
 
     
 root.render(parent);
-console.log(parent)
+// console.log(parent)
