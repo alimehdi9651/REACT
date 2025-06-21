93e82d9d0555fcfaac30045f883023ed854524dd
alimehdi <alimehdi9651@gmail.com>
2025-06-21 10:58:18 +0530

    ep 6

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,99 +1,91 @@
 import RestaurantCard from "./RestauantCard";
-import resList from "../utils/mockData";
-import {useState } from "react";
+
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 const Body = () => {
-  //state variable
+  //local state variable = supwe powerful variable
   //if you want to modify this listofresaurant then we have to pass the function as well
   // whatever modification we want in the list we have to pass inside the function as argument
-  const [listOfRestaurants, setListOfRestaurant] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurant] = useState([]);
+  const [filteredList, setFilteredList] = useState([]);
+  //defination:************** whenever state variable updates react triggers a reconciliation cycle or rerender the component again****************
   //normal js variable
-  let listOfRestaurantsJS = [
-    {
-      card: {
-        card: {
-          "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
-          info: {
-            id: "78675",
-            name: "Raj Luxmi Restaurant",
-            city: "15",
+  console.log("body render");
+  const [searchText, setSearchText] = useState("");
 
-            cloudinaryImageId:
-              "RX_THUMBNAIL/IMAGES/VENDOR/2025/5/9/da5e90cd-9a84-4c34-b000-9b957f8c324a_78675.jpg",
-            costForTwo: "30000",
-            costForTwoMessage: "₹300 FOR TWO",
-            cuisines: ["Indian", "South Indian", "Chinese", "Thalis"],
-            avgRating: 2,
-            avgRatingString: "2.0",
-            deliveryTime: 33,
-          
-          },
-        },
-      },
-    },
-    {
-      card: {
-        card: {
-          "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
-          info: {
-            id: "78676",
-            name: "KFC",
-            city: "15",
+  //useEffect(): Another State variable that will run after the whole body render
+  //it will take two argument one is the array and second is the array
+  useEffect(() => {
+    //whatever we write in the body this will run after the complete body runs and web page render
+    fetchData();
+  }, []);
 
-            cloudinaryImageId:
-              "RX_THUMBNAIL/IMAGES/VENDOR/2025/5/9/da5e90cd-9a84-4c34-b000-9b957f8c324a_78675.jpg",
-            costForTwo: "30000",
-            costForTwoMessage: "₹300 FOR TWO",
-            cuisines: ["Indian", "South Indian", "Chinese", "Thalis"],
-            avgRating: 4.5,
-            avgRatingString: "3.5",
-            deliveryTime: 33,
-          
-          },
-        },
-      },
-    },
-    {
-      card: {
-        card: {
-          "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
-          info: {
-            id: "78677",
-            name: "MCD",
-            city: "15",
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/search/v3?lat=26.86474977468873&lng=80.90554151684046&str=Restaurants%20near%20me&trackingId=bb6c58f7-2b4b-cd16-7aea-130e24913aee&submitAction=ENTER&queryUniqueId=6f70ce15-9b37-12d1-5d20-896ef790e8d3"
+    );
+    const json = await data.json();
+    // console.log(json);
+    setListOfRestaurant(
+      //Optional Channing
+      json?.data?.cards[1]?.groupedCard?.cardGroupMap?.RESTAURANT?.cards
+    );
+    setFilteredList(
+      //Optional Channing
+      json?.data?.cards[1]?.groupedCard?.cardGroupMap?.RESTAURANT?.cards
+    );
+    // const d = json.data.cards[1].groupedCard.cardGroupMap.RESTAURANT.cards;
+    // console.log(d);
+  };
 
-            cloudinaryImageId:
-              "RX_THUMBNAIL/IMAGES/VENDOR/2025/5/9/da5e90cd-9a84-4c34-b000-9b957f8c324a_78675.jpg",
-            costForTwo: "30000",
-            costForTwoMessage: "₹300 FOR TWO",
-            cuisines: ["Indian", "South Indian", "Chinese", "Thalis"],
-            avgRating: 4.1,
-            avgRatingString: "4.1",
-            deliveryTime: 33,
-          
-          },
-        },
-      },
-    },
-  ];
-  return (
+  //Conditional Renderig
+  // if (listOfRestaurants.length == 0) {
+  //   return <Shimmer />;
+  // }
+  return listOfRestaurants.length == 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              // filter restaurants and update UI
+              const filterRestaurant = listOfRestaurants.filter((res) =>
+                res.card.card.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              // console.log(res.card.card.info.name)
+
+              setFilteredList(filterRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
+
         <button
           className="filter-btn"
           onClick={() => {
-            
-            const filtered_list= listOfRestaurants.filter(
-              (res) => res.card.card.info.avgRating > 4
+            const filtered_list = filteredList.filter(
+              (res) => res.card.card.info.avgRating >= 4.5
             );
-            setListOfRestaurant(filtered_list)
-            console.log(listOfRestaurants)
+            setFilteredList(filtered_list);
+            // console.log(listOfRestaurants);
           }}
         >
           Top rated Restaurant
         </button>
       </div>
       <div className="res-container">
-        {listOfRestaurants.map((restaurant) => (
+        {filteredList.map((restaurant) => (
           <RestaurantCard
             key={restaurant.card.card.info.id}
             resData={restaurant}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,32 @@
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
 const Header = () => {
-    return (
-      <div className="header">
-        <div className="logo-container">
-          <img
-            className="logo"
-            src= {LOGO_URL} />
-        </div>
-        <div className="nav-items">
-          <ul>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Contact Us</li>
-            <li>Cart</li>
-          </ul>
-        </div>
+  const [btnLogin, setBtnLogin] = useState("Login");
+  console.log("Render ho gaya")
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL} />
       </div>
-    );
-  };
-export default Header
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btnLogin === "Login"
+                ? setBtnLogin("Logout")
+                : setBtnLogin("Login");
+            }}
+          >
+            {btnLogin}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default Header;
